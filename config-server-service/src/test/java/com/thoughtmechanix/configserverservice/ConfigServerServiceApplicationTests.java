6a76cf1beffb401ea68292b6dcfec1d4e74041fb
Robin <sharmarobin.sr@gmail.com>
2020-06-24 10:17:47 +0530

    Initial Commit&Push for Config-Server code.

---
+++ config-server-service/src/test/java/com/thoughtmechanix/configserverservice/ConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.configserverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
